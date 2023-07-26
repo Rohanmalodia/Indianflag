b75d8bcfff7f66ae5fd10d69f9b6e861177ee9f8
Rohanmalodia <55531074+Rohanmalodia@users.noreply.github.com>
2023-07-27 00:16:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function init_threeScene(spec) {
 
       const faceMesh = new THREE.Mesh(geometry, mat);
       faceMesh.position.y -= 0.10;
-      faceMesh.position.z -= 0.70;
+      faceMesh.position.z -= 0.25;
       faceMesh.position.x += 0;
 
       addDragEventListener(faceMesh);
